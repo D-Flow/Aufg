652403fd083211fb82d436c5a23238e593b85116
basti <u>
2017-04-20 23:07:48 +0200

    ggt blatt1

--- DAP_KurzAufgaben/Blatt1/Euclid.java
+++ DAP_KurzAufgaben/Blatt1/Euclid.java
@@ -3,5 +3,13 @@
  */
 public class Euclid {
     public static void main(String...args){
+
+    }
+
+    public static int euclid(int a,int b){
+        if(b==0)
+            return a;
+        else
+            return euclid(b,a%b);
     }
 }
