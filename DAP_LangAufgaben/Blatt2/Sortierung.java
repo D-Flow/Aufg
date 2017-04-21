4221ccf11fa125938a67d729e4aabc3e2f133812
basti <u>
2017-04-21 21:31:41 +0200

    Merge Methode mit linearer Zeit und zur übersicht
    Signed-off-by: basti <u>

--- DAP_LangAufgaben/Blatt2/Sortierung.java
+++ DAP_LangAufgaben/Blatt2/Sortierung.java
@@ -1,11 +1,12 @@
 import java.util.Random;
 
+
 /**
  *
  */
 public class Sortierung {
     public static void main(String... args) {
-
+        mergeSort(fillRandom(1000));
     }
 
     public static void insertionSort(int[] array) {
@@ -66,6 +67,34 @@ public class Sortierung {
         return arr;
     }
 
+    private static void merge(int[] array, int[] tempArray, int left, int middle, int right) {
+        for (int i = left; i <= right; i++)  // kopieren damit beim zusammenfügen nichts verloren wird   => wähle immer aus tmp
+            tempArray[i] = array[i];
+
+        int rightPointer = middle + 1, leftPointer = left;  //Pointer auf die jeweils aktuellen elemente im linken bzw. rechten teil array
+        for (int currentI = left; currentI <= right; currentI++) {
+            if (leftPointer <= middle && rightPointer <= right)     // links und rechts sind valide möglichkeiten => wähle kleinste
+                if (tempArray[leftPointer] <= tempArray[rightPointer]) {
+                    array[currentI] = tempArray[leftPointer]; //linke hälfte besitzt kleineres element => wähle es und schiebe pointer 1 weiter
+                    leftPointer++;
+                } else {
+                    array[currentI] = tempArray[rightPointer];  // analog mit wähle rechtes element
+                    rightPointer++;
+                }
+            else {//wenn eintrifft wird nur noch dies ausgeführt da der invalide pointer niemals valid wird!
+
+                //nur ein Pointer ist valid => wähle den validen
+                if (leftPointer <= middle) {
+                    array[currentI] = tempArray[leftPointer];
+                    leftPointer++;
+                } else {
+                    array[currentI] = tempArray[rightPointer];
+                    rightPointer++;
+                }
+            }
+            assert isPartialSorted(array, left, currentI);
+        }
+    }
     public static void mergeSort(int[] array,int[] tempArray,int left,int right){
         if(left>=right)
             return;
@@ -74,32 +103,7 @@ public class Sortierung {
         mergeSort(array,tempArray,q+1,right);
         assert isPartialSorted(array,left,q);   // linke hälfte ist sortiert
         assert isPartialSorted(array,q+1,right);    // rechte hälfte ist sortiert
-        for(int i = left;i<=right;i++)  // kopieren damit beim zusammenfügen nichts verloren wird   => wähle immer aus tmp
-            tempArray[i]=array[i];
-
-        int rightPointer=q+1,leftPointer=left;  //Pointer auf die jeweils aktuellen elemente im linken bzw. rechten teil array
-        for(int currentI=left;currentI<=right;currentI++){
-            if(leftPointer<=q&&rightPointer<=right)     // links und rechts sind valide möglichkeiten => wähle kleinste
-                if(tempArray[leftPointer]<=tempArray[rightPointer]){
-                    array[currentI]=tempArray[leftPointer]; //linke hälfte besitzt kleineres element => wähle es und schiebe pointer 1 weiter
-                    leftPointer++;
-                }else {
-                    array[currentI] = tempArray[rightPointer];  // analog mit wähle rechtes element
-                    rightPointer++;
-                }
-            else{//wenn eintrifft wird nur noch dies ausgeführt da der invalide pointer niemals valid wird!
-
-                    //nur ein Pointer ist valid => wähle den validen
-                if(leftPointer<=q){
-                    array[currentI]=tempArray[leftPointer];
-                    leftPointer++;
-                }else{
-                    array[currentI] = tempArray[rightPointer];
-                    rightPointer++;
-                }
-            }
-            assert isPartialSorted(array,left,currentI);
-        }
+        merge(array, tempArray, left, q, right);    //assert passiert im aufrufendem call! auf left bis right
     }
 
     public static void mergeSort(int[] array) {
